1f414774e56f058037da201370f2135deb299cf0
sean kallipolites <skallipo@gmail.com>
2022-03-07 21:30:16 -0500

    more form development, started adding JS for form flow

--- js/scripts.js
+++ js/scripts.js
@@ -8,103 +8,35 @@
     collapsableNavbar.toggle();
 } */
 
-let step = document.getElementsByClassName('step');
-let prevBtn = document.getElementById('prev-btn');
-let nextBtn = document.getElementById('next-btn');
-let submitBtn = document.getElementById('submit-btn');
-let form = document.getElementsByTagName('form')[0];
-let preloader = document.getElementById('preloader-wrapper');
-let bodyElement = document.querySelector('body');
-let succcessDiv = document.getElementById('success');
+var form1 = document.getElementById("formPage1");
+var form2 = document.getElementById("formPage2");
+var form3 = document.getElementById("formPage3");
+var form4 = document.getElementById("formPage4");
 
-form.onsubmit = () => {
-    return false
-}
-let current_step = 0;
-let stepCount = 4
-step[current_step].classList.add('d-block');
-if (current_step == 0) {
-    prevBtn.classList.add('d-none');
-    submitBtn.classList.add('d-none');
-    nextBtn.classList.add('d-inline-block');
+var nextButton1 = document.getElementById("nextButton1");
+var prevButton1 = document.getElementById("prevButton1");
+
+form2.hidden = true;
+form3.hidden = true;
+form4.hidden = true;
+
+
+
+nextButton1.addEventListener("click", function () { 
+    nextForm(form1, form2);
+});
+
+
+prevButton1.addEventListener("click", function () { 
+    prevForm(form1, form2);
+});
+
+function nextForm(f1, f2) {
+    f1.hidden = true;
+    f2.hidden = false;
 }
 
-const progress = (value) => {
-    document.getElementsByClassName('progress-bar')[0].style.width = `${value}%`;
+function prevForm(f1, f2) {
+    f1.hidden = false;
+    f2.hidden = true;
 }
-
-nextBtn.addEventListener('click', () => {
-    current_step++;
-    let previous_step = current_step - 1;
-    if ((current_step > 0) && (current_step <= stepCount)) {
-        prevBtn.classList.remove('d-none');
-        prevBtn.classList.add('d-inline-block');
-        step[current_step].classList.remove('d-none');
-        step[current_step].classList.add('d-block');
-        step[previous_step].classList.remove('d-block');
-        step[previous_step].classList.add('d-none');
-        if (current_step == stepCount) {
-            submitBtn.classList.remove('d-none');
-            submitBtn.classList.add('d-inline-block');
-            nextBtn.classList.remove('d-inline-block');
-            nextBtn.classList.add('d-none');
-        }
-    } else {
-        if (current_step > stepCount) {
-            form.onsubmit = () => {
-                return true
-            }
-        }
-    }
-    progress((100 / stepCount) * current_step);
-});
- 
- 
-prevBtn.addEventListener('click', () => {
-    if (current_step > 0) {
-        current_step--;
-        let previous_step = current_step + 1;
-        prevBtn.classList.add('d-none');
-        prevBtn.classList.add('d-inline-block');
-        step[current_step].classList.remove('d-none');
-        step[current_step].classList.add('d-block')
-        step[previous_step].classList.remove('d-block');
-        step[previous_step].classList.add('d-none');
-        if (current_step < stepCount) {
-            submitBtn.classList.remove('d-inline-block');
-            submitBtn.classList.add('d-none');
-            nextBtn.classList.remove('d-none');
-            nextBtn.classList.add('d-inline-block');
-            prevBtn.classList.remove('d-none');
-            prevBtn.classList.add('d-inline-block');
-        }
-    }
- 
-    if (current_step == 0) {
-        prevBtn.classList.remove('d-inline-block');
-        prevBtn.classList.add('d-none');
-    }
-    progress((100 / stepCount) * current_step);
-});
- 
- 
-submitBtn.addEventListener('click', () => {
-    preloader.classList.add('d-block');
- 
-    const timer = ms => new Promise(res => setTimeout(res, ms));
- 
-    timer(3000)
-        .then(() => {
-            bodyElement.classList.add('loaded');
-        }).then(() => {
-            step[stepCount].classList.remove('d-block');
-            step[stepCount].classList.add('d-none');
-            prevBtn.classList.remove('d-inline-block');
-            prevBtn.classList.add('d-none');
-            submitBtn.classList.remove('d-inline-block');
-            submitBtn.classList.add('d-none');
-            succcessDiv.classList.remove('d-none');
-            succcessDiv.classList.add('d-block');
-        })
- 
-});
